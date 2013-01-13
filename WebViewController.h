5b21a8cc88182f1632460faff2a70a18ed1cfc6a
Satoshi Ebina <P06019@ebinasatoshi.local>
2013-01-13 19:55:43 +0900

    added the back ground image

---
+++ WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  toilet
+//
+//  Created by Satoshi Ebina on 13/01/13.
+//  Copyright (c) 2013年 Satoshi Ebina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
---
+++ WebViewController.m
@@ -0,0 +1,38 @@
+//
+//  WebViewController.m
+//  toilet
+//
+//  Created by Satoshi Ebina on 13/01/13.
+//  Copyright (c) 2013年 Satoshi Ebina. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
